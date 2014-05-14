cef08659bae98618db404e082258b3f7f1433190
Patrick Spek <p.spek@tyil.nl>
2014-05-14 08:46:45 +0200

    Added more options, some comments and a rename

--- tachikoma.rb
+++ tachikoma.rb
@@ -9,9 +9,21 @@ Dir[File.dirname(__FILE__) + '/plugins/*.rb'].each {|file| require file}
 # Create the bot instance
 bot = Cinch::Bot.new do
   configure do |c|
+    # Server config
     c.server          = "irc.rizon.net"
+    c.port            = 6667
+    c.nick            = "tachikoma"
+    c.user            = "cinch"
+    c.realname        = "Tachikoma"
     c.channels        = ["#scriptkitties"]
-    c.nick            = "not-a-bot"
+
+    # SSL config
+    c.ssl.ca_path     = "/etc/ssl/certs/"
+    c.ssl.client_cert = ""
+    c.ssl.use         = true
+    c.ssl.verify      = true
+
+    # Loaded plugins
     c.plugins.plugins = [
       Controls,
       Social
@@ -19,13 +31,16 @@ bot = Cinch::Bot.new do
   end
 
   trap "SIGINT" do
+    # Exit cleanly
     bot.quit
   end
 
   trap "SIGTERM" do
+    # Exit cleanly
     bot.quit
   end
 end
 
 # Start the bot
 bot.start
+
