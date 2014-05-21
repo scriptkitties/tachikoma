36c8e0425c72c064054cc57ffab59c2c9c4aa5d2
Patrick "Tyil" Spek <p.spek@tyil.nl>
2014-05-21 18:43:28 +0200

    :hello case insensitive, :goodbye works properly

--- plugins/social.rb
+++ plugins/social.rb
@@ -5,13 +5,13 @@ require "cinch"
 class Social
   include Cinch::Plugin
 
-  match(/^bye/, use_prefix: false)
-  def execute(m)
+  match(/^bye/i, method: :goodbye, use_prefix: false)
+  def goodbye(m)
     m.reply "Goodbye, #{m.user.nick}"
   end
 
-  match(/^hello$|^hi$/, use_prefix: false)
-  def execute(m)
+  match(/^hello$|^hi$/i, method: :hello, use_prefix: false)
+  def hello(m)
     m.reply "Hello, #{m.user.nick}!"
   end
 end
