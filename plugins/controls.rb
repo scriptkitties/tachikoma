6e058f2a0f0778af3050e7d7453d7fd8220091ce
Robin Rijkeboer <tunderofnl@gmail.com>
2014-05-22 22:45:19 +0200

    Add Part (Leave channel) and Join (Join channel) command

--- plugins/controls.rb
+++ plugins/controls.rb
@@ -10,15 +10,30 @@ class Controls
   match(/join (S+)/)
   def execute(m, channel)
     bot.join channel
+  match(/join (\w+)/i, method: :join)
+  def join(m, channel)
+    adminArray = ['tyiwi', 'Beagon', 'tyil']
+    if adminArray.include? m.user.nick
+     channelAppend = "#"
+     channelAppend += channel
+     bot.join channelAppend
+   end
   end
 
   # todo: invite - Let the bot invite someone (and give this nick +v when it joins)
   # todo: kick - Let the bot kick someone
-  # todo: part - Leave current channel
+  match(/part (\w+)/i, method: :part)
+  def part(m, channel)
+    adminArray = ['tyiwi', 'Beagon', 'tyil']
+    if adminArray.include? m.user.nick
+     channelAppend = "#"
+     channelAppend += channel
+     bot.part channelAppend
+   end
+  end
 
   match "quit"
   def execute(m)
     bot.quit
   end
 end
-
