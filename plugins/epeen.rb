33f751f2b150e4876c6774e6f2ceb0d9b0dbc4cf
Patrick "Tyil" Spek <p.spek@tyil.nl>
2014-05-22 08:35:29 +0200

    updated epeen

--- plugins/epeen.rb
+++ plugins/epeen.rb
@@ -5,36 +5,33 @@ require "cinch"
 class Epeen
   include Cinch::Plugin
 
-  match(/extend (\w+)/i, method: :extend)
-  def extend(m, nick)
+  def mutatePeen(m, nick, value)
     c = UserModel.filter(Sequel.ilike(:nickname, nick)).count
 
     if c == 0
-      m.reply "No"
-    else
-      u = UserModel.select(:id, :nickname).filter(Sequel.ilike(:nickname, nick)).all
-      s = UserStatModel.where(:user_id => u[0].id).all
-      s[0].epeen += 1
-      s[0].save
-
-      m.reply "Epeen of #{u[0].nickname} is now #{s[0].epeen}"
+      return false
     end
+
+    u = UserModel.select(:id, :nickname).filter(Sequel.ilike(:nickname, nick)).all
+    s = UserStatModel.where(:user_id => u[0].id).all
+    s[0].epeen += value
+    s[0].save
+
+    m.reply "Epeen of #{u[0].nickname} is now #{s[0].epeen}"
+
+    return true
   end
 
+  match(/^\+(\w+)/i, method: :extend, use_prefix: false)
+  match(/extend (\w+)/i, method: :extend)
+  def extend(m, nick)
+    mutatePeen(m, nick, 1)
+  end
+
+  match(/^\-(\w+)/i, method: :shrink, use_prefix: false)
   match(/shrink (\w+)/i, method: :shrink)
   def shrink(m, nick)
-    c = UserModel.filter(Sequel.ilike(:nickname, nick)).count
-
-    if c == 0
-      m.reply "No"
-    else
-      u = UserModel.select(:id, :nickname).filter(Sequel.ilike(:nickname, nick)).all
-      s = UserStatModel.where(:user_id => u[0].id).all
-      s[0].epeen -= 1
-      s[0].save
-
-      m.reply "Epeen of #{u[0].nickname} is now #{s[0].epeen}"
-    end
+    mutatePeen(m, nick, -1)
   end
 end
 
