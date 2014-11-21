fd43a5022e6c1a02aedebfcf4af051f6424ed6dd
Patrick Spek <p.spek@tyil.nl>
2014-11-21 09:42:12 +0100

    minor cleanup to the Social plugin

--- plugins/social.rb
+++ plugins/social.rb
@@ -6,11 +6,12 @@ class Social
   include Cinch::Plugin
 
   match(/^bye/i, method: :goodbye, use_prefix: false)
+  match(/^hello$|^hi$/i, method: :hello, use_prefix: false)
+
   def goodbye(m)
     m.reply "Goodbye, #{m.user.nick}"
   end
 
-  match(/^hello$|^hi$/i, method: :hello, use_prefix: false)
   def hello(m)
     m.reply "Hello, #{m.user.nick}!"
   end
